81c4b0b300841ec000f3255606a3de3239da870a
Adrian Toczydłowski <adrian.toczydlowski@onestepcloud.com>
2016-04-28 11:20:38 +0200

    add CustomEvent function

--- build/guid3.js
+++ build/guid3.js
@@ -832,7 +832,8 @@ module.exports = function module(cb){
   */
   this.setValue = function(v){
     // update the target value
-    function CustomEvent ( event, params ) {
+    if ( typeof window.CustomEvent !== "function" ) {
+      function CustomEvent ( event, params ) {
         params = params || { bubbles: false, cancelable: false, detail: undefined };
         var evt = document.createEvent( 'CustomEvent' );
         evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
@@ -842,9 +843,8 @@ module.exports = function module(cb){
       CustomEvent.prototype = window.Event.prototype;
 
       window.CustomEvent = CustomEvent;
-    console.log('g_root', this.g_root);
-    console.log('this.g_root.node()', this.g_root.node());
-    console.log('custom event', new CustomEvent('changed', { detail:v } ));
+    }
+
     this.object_reference[self.object_key] = v
     this.g_root.node().dispatchEvent(new CustomEvent('changed', { detail:v } ))
   }
